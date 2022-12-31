4a9b6ebe3af72e4d8a2c5cb2a9889c9a622a1469
Lokianer <Lokianer@gmail.com>
2022-12-31 16:58:46 +0100

    Bot

--- bot.py
+++ bot.py
@@ -3,10 +3,9 @@ from discord.ext import tasks
 import tweepy
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
 DISCORD_API_KEY = os.getenv("DISCORD_API_KEY")
-
-client = discord.Client()
 
 TWITTER_CONSUMER_KEY = os.getenv("TWITTER_CONSUMER_KEY")
 TWITTER_CONSUMER_SECRET = os.getenv("TWITTER_CONSUMER_SECRET")
@@ -20,30 +19,41 @@ twitter_api = tweepy.API(auth)
 
 latest_tweet = None
 
+class MyClient(discord.Client):
+    # The function that will be called when the bot is ready
+    async def on_ready(self):
+        print('Logged on as', self.user)
+        tweets = twitter_api.user_timeline(screen_name=user)
 
-@client.event
-async def on_ready():
-    tweets = twitter_api.user_timeline(screen_name=user)
+        global latest_tweet
+        latest_tweet = tweets[0]
 
-    global latest_tweet
-    latest_tweet = tweets[0]
+        channel = client.get_channel() # Replace with the ID of the channel you want to send the message to
+        await channel.send('https://twitter.com/twitter/statuses/' + str(latest_tweet.id))
+    # A hook that will be called when the bot is ready
+    async def setup_hook(self) -> None:
+        self.check_tweets.start()
+    
 
-    channel = client.get_channel() # Replace with the ID of the channel you want to send the message to
-    await channel.send(latest_tweet.text)
+    @tasks.loop(seconds=10)
+    async def check_tweets(self):
+        tweets = twitter_api.user_timeline(screen_name=user)
 
-@tasks.loop(seconds=10)
-async def check_tweets():
-    tweets = twitter_api.user_timeline(screen_name=user)
+        latest_tweet_new = tweets[0]
 
-    latest_tweet_new = tweets[0]
+        global latest_tweet
+        if latest_tweet_new != latest_tweet:
+            channel = client.get_channel() 
+            await channel.send(latest_tweet_new.text)
 
-    global latest_tweet
-    if latest_tweet_new != latest_tweet:
-        channel = client.get_channel() 
-        await channel.send(latest_tweet_new.text)
+            latest_tweet = latest_tweet_new
+    @check_tweets.before_loop
+    async def before_check_tweets(self):
+        await self.wait_until_ready()
+        print('Finished waiting')
 
-        latest_tweet = latest_tweet_new
-
-check_tweets.start()
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
 
 client.run(DISCORD_API_KEY)
